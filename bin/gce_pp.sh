1f3fc80b7bd16ef4259bb537731a6fdcf62dd485
Marco Capuccini <m.capuccini@gmail.com>
2017-06-14 10:03:39 +0200

    Change comment [skip ci]

--- bin/gce_pp.sh
+++ bin/gce_pp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This Script will be executed as post-processor of the GCE packer builder.
 
-#Setting acls to public for new created image in Google storage so that it can be imported
+# Setting acls to public for new created image in Google storage so that it can be imported
 echo "Setting new created Google image as shared publicly..."
 gsutil acl ch -u AllUsers:R gs://kubenow-images/kubenow-"$CURRENT_VERSION".tar.gz
