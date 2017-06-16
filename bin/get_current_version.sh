9b11e02311ceaf9eabda87ed0d7edc9f25f2f17b
carmat88 <matteo.carone@farmbio.uu.se>
2017-06-16 15:16:40 +0200

    Final revisions-a1

--- bin/get_current_version.sh
+++ bin/get_current_version.sh
@@ -2,10 +2,7 @@
 # This script will extract the current version based on the github repository's tags
 
 # Get current_version
-export CURRENT_VERSION
 CURRENT_VERSION=v$(git describe --tags --always | tr -d .)
-
-export KUBENOW_REGEX
 KUBENOW_REGEX='^v[0-9]{3}([ab][0-9]{1,}|rc[0-9]{1,})?$'
 
 # Checking whether or not current version is tagged as release
@@ -16,6 +13,6 @@ elif [ "$TRAVIS_BRANCH" == 'master' ]; then
 else
     export CURRENT_VERSION="${CURRENT_VERSION}-test"
 fi
-
 echo "KubeNow image version is: $CURRENT_VERSION"
+
 export CURRENT_VERSION
