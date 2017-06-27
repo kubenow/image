155d887da55691c312af998ed089c8a7773ae6aa
Anders Larsson <anders.larsson@icm.uu.se>
2017-06-27 09:16:23 +0200

    pre-pull essential kubernetes docker images

--- requirements.sh
+++ requirements.sh
@@ -65,5 +65,16 @@ sudo chmod 0755 /usr/local/bin/heketi-cli
 rm -R /tmp/heketi-client/
 
 echo "Pulling required Docker images..."
+# Essential Kubernetes containers are listed in 
+# https://github.com/kubernetes/kubernetes/blob/master/cmd/kubeadm/app/images/images.go
+KUBE_VERSION="v1.6.4"
+ETCD_VERSION="3.0.17"
 sudo docker pull \
-  kubenow/gluster-server:0.1.0
+  gcr.io/google_containers/kube-apiserver-amd64:$KUBE_VERSION \
+  gcr.io/google_containers/kube-proxy-amd64:$KUBE_VERSION \
+  gcr.io/google_containers/kube-controller-manager-amd64:$KUBE_VERSION \
+  gcr.io/google_containers/kube-scheduler-amd64:$KUBE_VERSION
+  gcr.io/google_containers/etcd-amd64:$ETCD_VERSION
+  gcr.io/google_containers/pause-amd64:3.0
+
+kubenow/gluster-server:0.1.0
